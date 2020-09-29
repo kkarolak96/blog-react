8304ff3cbcc1dcda61d65f88da799b6b1146bf27
Kamil Karolak <kkarolak96@gmail.com>
2020-09-29 12:48:04 +0200

    initialize commit

---
+++ src/actions/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash'
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    /*  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+      userIds.forEach(id => dispatch(fetchUser(id)))*/
+    // Równoważne temu powyżej - funkcja _.chain()
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+
+};
+
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts')
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+}
+
+export const fetchUser = (userId) => async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`)
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+}
+
+
+
+
+// Memoized version - overfetching 
+/*export const fetchUser = (userId) => (dispatch) =>  _fetchUser(userId, dispatch);
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`)
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+});*/
---
+++ src/components/PostList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts, fetchPostsAndUsers } from '../actions'
+import UserHeader from './UserHeader'
+
+class PostList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPostsAndUsers();
+    }
+
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <h2>{post.body}</h2>
+                            <h2><UserHeader userId={post.userId} /></h2>
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+
+    render() {
+        return <div className="ui relaxed divided list">{this.renderList()}</div>
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList)
